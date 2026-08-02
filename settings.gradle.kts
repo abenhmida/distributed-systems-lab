94d14f5c815fb24e62940c5f35c1334199786759
abenhmida <aymen.benhmida@gmail.com>
2026-08-02 15:04:37 +0200

    ADd gradle catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,26 @@ rootProject.name = "distributed-systems-lab"
 
 include(
     "shared:common",
+    "shared:testing",
+
     "services:order-service",
 )
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    includeBuild("build-logic")
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
+    }
+}
